35e69fc60f3274d1869e5d6d86c855d3d24fe419
Kevnroux <kevroux3@utexas.edu>
2023-08-10 02:57:07 -0400

    Updated pathing to find BOINC in src folder for new server build

--- cmake/Modules/FindBOINC.cmake
+++ cmake/Modules/FindBOINC.cmake
@@ -14,9 +14,6 @@ ENDIF (BOINC_INCLUDE_DIR)
 
 FIND_PATH(BOINC_INCLUDE_DIR api/boinc_api.h
     /boinc/src/boinc
-    /home/tdesell/boinc
-    /Users/deselt/Software/boinc
-    /boinc/src/BOINCBuild1.0.4/boinc
     $ENV{BOINC_SOURCE}
 )
 MESSAGE(STATUS "BOINC include directory: ${BOINC_INCLUDE_DIR}")
@@ -25,28 +22,28 @@ MESSAGE(STATUS "BOINC source: $ENV{BOINC_SOURCE}")
 
 FIND_LIBRARY(BOINC_LIBRARY
     NAMES boinc
-    PATHS /boinc/src/boinc /home/tdesell/boinc /Users/deselt/Software/boinc/mac_build/build/Deployment/ /boinc/src/BOINCBuild1.0.4/boinc $ENV{BOINC_SOURCE}
+    PATHS /boinc/src/boinc  $ENV{BOINC_SOURCE}
     PATH_SUFFIXES lib
 )
 MESSAGE(STATUS "BOINC library: ${BOINC_LIBRARY}")
 
 FIND_LIBRARY(BOINC_CRYPT_LIBRARY
     NAMES boinc_crypt
-    PATHS /boinc/src/boinc /home/tdesell/boinc /Users/deselt/Software/boinc/mac_build/build/Deployment/ /boinc/src/BOINCBuild1.0.4/boinc $ENV{BOINC_SOURCE}
+    PATHS /boinc/src/boinc  $ENV{BOINC_SOURCE}
     PATH_SUFFIXES lib
 )
 MESSAGE(STATUS "BOINC crypt library: ${BOINC_CRYPT_LIBRARY}")
 
 FIND_LIBRARY(BOINC_API_LIBRARY
     NAMES boinc_api
-    PATHS /boinc/src/boinc /home/tdesell/boinc /Users/deselt/Software/boinc/mac_build/build/Deployment/ /boinc/src/BOINCBuild1.0.4/boinc $ENV{BOINC_SOURCE}
+    PATHS /boinc/src/boinc  $ENV{BOINC_SOURCE}
     PATH_SUFFIXES api
 )
 MESSAGE(STATUS "BOINC api library: ${BOINC_API_LIBRARY}")
 
 FIND_LIBRARY(BOINC_SCHED_LIBRARY
     NAMES sched
-    PATHS /boinc/src/boinc /home/tdesell/boinc /Users/deselt/Software/boinc/mac_build/build/Deployment/ /boinc/src/BOINCBuild1.0.4/boinc $ENV{BOINC_SOURCE}
+    PATHS /boinc/src/boinc  $ENV{BOINC_SOURCE}
     PATH_SUFFIXES sched
 )
 MESSAGE(STATUS "BOINC sched library: ${BOINC_SCHED_LIBRARY}")
